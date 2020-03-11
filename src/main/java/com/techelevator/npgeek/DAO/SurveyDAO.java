8b4dd09957340333c4c785864b6377b0efe86d88
Prakash Adhikari <prakashadh7@gmail.com>
2020-03-11 15:58:26 -0400

    JDBC park and surveyResults completed

--- src/main/java/com/techelevator/npgeek/DAO/SurveyDAO.java
+++ src/main/java/com/techelevator/npgeek/DAO/SurveyDAO.java
@@ -1,8 +1,7 @@
 package com.techelevator.npgeek.DAO;
 
 import java.util.List;
-import com.techelevator.npgeek.Survey;
-import com.techelevator.npgeek.SurveyResults;
+import com.techeevator.model.SurveyResults;
 
 public interface SurveyDAO {
 	List <SurveyResults> getAllSurvey ();
